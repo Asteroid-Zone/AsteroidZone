895d2b19da1c255ee183ca5dcc86a768088a3ab5
Milen Georgiev <milendg99@gmail.com>
2021-02-02 18:29:10 +0200

    Initial Commit with bare web structure skeleton.

---
+++ AsteroidZone/Pages/Play.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AsteroidZone.Pages
+{
+    public class PlayModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
