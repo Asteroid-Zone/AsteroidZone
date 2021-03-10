ef1561c6b6d8c2e21ff13df35e579198e3c31caf
Milen Georgiev <milendg99@gmail.com>
2021-03-10 18:39:37 +0200

    Added initial code

--- AsteroidZone/Startup.cs
+++ AsteroidZone/Startup.cs
@@ -3,6 +3,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Cloud.Speech.V1;
+using Microsoft.AspNetCore.Http;
 
 namespace AsteroidZone
 {
@@ -43,6 +50,29 @@ namespace AsteroidZone
             });
 
             app.UseRouting();
+            app.UseWebSockets();
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        using (WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync())
+                        {
+                            await GoogleCloudVoiceRec(context, webSocket);
+                        }
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+
+            });
 
             app.UseAuthorization();
 
@@ -50,6 +80,84 @@ namespace AsteroidZone
             {
                 endpoints.MapRazorPages();
             });
+        }
+
+        static async Task GoogleCloudVoiceRec(HttpContext context, WebSocket webSocket)
+        {
+            // Create a speech client using the credentials
+            var speechBuilder = new SpeechClientBuilder
+            {
+                CredentialsPath = ".\\key.json"
+            };
+
+            var speech = speechBuilder.Build();
+            var streamingCall = speech.StreamingRecognize();
+
+            // Write the initial request with the config of the audio
+            await streamingCall.WriteAsync(
+                new StreamingRecognizeRequest()
+                {
+                    StreamingConfig = new StreamingRecognitionConfig()
+                    {
+                        Config = new RecognitionConfig()
+                        {
+                            Encoding =
+                            RecognitionConfig.Types.AudioEncoding.Linear16,
+                            SampleRateHertz = 16000,
+                            LanguageCode = "en-GB",
+                        },
+                        InterimResults = true,
+                    }
+                });
+
+
+            // Print responses as they arrive - this is supposed to be the recognised text sent back from google's servers
+            Task printResponses = Task.Run(async () =>
+            {
+                var responseStream = streamingCall.GetResponseStream();
+                while (await responseStream.MoveNextAsync())
+                {
+                    StreamingRecognizeResponse response = responseStream.Current;
+                    foreach (StreamingRecognitionResult recognitionResult in response.Results)
+                    {
+                        foreach (SpeechRecognitionAlternative alternative in recognitionResult.Alternatives)
+                        {
+                            // Print the result on the console and send back via the websocket
+                            Console.WriteLine(alternative.Transcript);
+                            await SendString((ClientWebSocket)webSocket, alternative.Transcript, CancellationToken.None);
+                        }
+                    }
+                }
+            });
+
+            // Create a buffer which will be used to 
+            var buffer = new byte[1024 * 4];
+
+            WebSocketReceiveResult result;
+            do
+            {
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                await streamingCall.WriteAsync(
+                    new StreamingRecognizeRequest()
+                    {
+                        AudioContent = Google.Protobuf.ByteString
+                            .CopyFrom(buffer, 0, buffer.Length),
+                    });
+            } while (!result.CloseStatus.HasValue);
+            
+            // Close the socket when all of the data has been taken
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+
+            await streamingCall.WriteCompleteAsync();
+            await printResponses;
+        }
+
+        public static Task SendString(ClientWebSocket ws, string data, CancellationToken cancellation)
+        {
+            var encoded = Encoding.UTF8.GetBytes(data);
+            var buffer = new ArraySegment<byte>(encoded, 0, encoded.Length);
+            return ws.SendAsync(buffer, WebSocketMessageType.Text, true, cancellation);
         }
     }
 }
--- AsteroidZone/wwwroot/js/SpeechRecognitionScript.js
+++ AsteroidZone/wwwroot/js/SpeechRecognitionScript.js
@@ -1,69 +1,96 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
-    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-    const SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
+﻿//document.addEventListener('DOMContentLoaded', function () {
+//    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+//    const SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
 
-    if (!SpeechRecognition) {
-        return;
-    }
+//    if (!SpeechRecognition) {
+//        return;
+//    }
 
-    // Attach recognition object to window so that it can be accessed outside of this scope (at start and stop recognition functions called by Unity)
-    window.recognition = new SpeechRecognition();
-    const speechRecognitionList = new SpeechGrammarList();
+//    // Attach recognition object to window so that it can be accessed outside of this scope (at start and stop recognition functions called by Unity)
+//    window.recognition = new SpeechRecognition();
+//    const speechRecognitionList = new SpeechGrammarList();
 
-    // Check https://developer.syn.co.in/tutorial/speech/jsgf-grammar.html
-    const commands = ['pirate', 'pirates', 'asteroid', 'asteroids', 'ping', 'pin', 'at', 'north', 'south', 'east', 'west', 'go', 'move', 'to', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    const grammar = `#JSGF V1.0; grammar commands; public <command> = (${commands.join(' | ')} );`;
+//    // Check https://developer.syn.co.in/tutorial/speech/jsgf-grammar.html
+//    const commands = ['pirate', 'pirates', 'asteroid', 'asteroids', 'ping', 'pin', 'at', 'north', 'south', 'east', 'west', 'go', 'move', 'to', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+//    const grammar = `#JSGF V1.0; grammar commands; public <command> = (${commands.join(' | ')} );`;
 
-    speechRecognitionList.addFromString(grammar, 1);
-    recognition.grammars = speechRecognitionList;
-    recognition.continuous = true;
-    recognition.lang = 'en-Gb';
-    recognition.interimResults = true;
-    recognition.maxAlternatives = 1;
+//    speechRecognitionList.addFromString(grammar, 1);
+//    recognition.grammars = speechRecognitionList;
+//    recognition.continuous = true;
+//    recognition.lang = 'en-Gb';
+//    recognition.interimResults = true;
+//    recognition.maxAlternatives = 1;
     
-    recognition.onresult = function (event) {
-        var interimTranscript = '';
-        for (let i = event.resultIndex; i < event.results.length; ++i) {
-            interimTranscript += event.results[i][0].transcript;
-        }
+//    recognition.onresult = function (event) {
+//        var interimTranscript = '';
+//        for (let i = event.resultIndex; i < event.results.length; ++i) {
+//            interimTranscript += event.results[i][0].transcript;
+//        }
 
-        unityInstance.SendMessage('CommandListener', 'GetResponse', interimTranscript);
-    }
+//        unityInstance.SendMessage('CommandListener', 'GetResponse', interimTranscript);
+//    }
 
-    recognition.onend = function () {
-        if (!flagStopRecognition) {
-            recognition.start();
-        }
-    }
+//    recognition.onend = function () {
+//        if (!flagStopRecognition) {
+//            recognition.start();
+//        }
+//    }
     
-    recognition.onerror = function (event) {
-        console.log("Error: " + event.error);
-    }
+//    recognition.onerror = function (event) {
+//        console.log("Error: " + event.error);
+//    }
 
-    recognition.onnomatch = () => {
-        console.log('No match!');
-    };
-});
+//    recognition.onnomatch = () => {
+//        console.log('No match!');
+//    };
+//});
 
-// A flag used when the recognition should be stopped and then the onend listener should not start again
-var flagStopRecognition = false;
+//// A flag used when the recognition should be stopped and then the onend listener should not start again
+//var flagStopRecognition = false;
 
-/**
- * Called within Unity to START the voice recognition process.
- */
-function startVoiceRecognition() {
-    if (window.recognition) {
-        flagStopRecognition = false;
-        recognition.start();
-    }
-}
+///**
+// * Called within Unity to START the voice recognition process.
+// */
+//function startVoiceRecognition() {
+//    if (window.recognition) {
+//        flagStopRecognition = false;
+//        recognition.start();
+//    }
+//}
 
-/**
- * Called within Unity to STOP the voice recognition process.
- */
-function stopVoiceRecognition() {
-    if (window.recognition) {
-        flagStopRecognition = true;
-        recognition.stop();
-    }
+///**
+// * Called within Unity to STOP the voice recognition process.
+// */
+//function stopVoiceRecognition() {
+//    if (window.recognition) {
+//        flagStopRecognition = true;
+//        recognition.stop();
+//    }
+//}
+
+const webSocket = new WebSocket('ws://localhost/AsteroidZone/ws');
+webSocket.binaryType = 'blob';
+
+webSocket.onopen = event => {
+    console.log('info: connected to server');
+
+    navigator.mediaDevices
+        .getUserMedia({ audio: true, video: false })
+        .then(stream => {
+            const mediaRecorder = new MediaRecorder(stream, {
+                mimeType: 'audio/webm',
+            });
+
+            mediaRecorder.addEventListener('dataavailable', event => {
+                if (event.data.size > 0) {
+                    webSocket.send(event.data);
+                }
+            });
+
+            mediaRecorder.start(1000);
+        });
+};
+
+webSocket.onmessage = (event) => {
+    console.log('WebSocket message received:', event);
 }
