901d0ae16b633c7b30bc1873beb0a9c8d949371c
Milen Georgiev <milendg99@gmail.com>
2021-03-17 00:38:31 +0200

    Improved website looks

--- AsteroidZone/Startup.cs
+++ AsteroidZone/Startup.cs
@@ -27,12 +27,14 @@ namespace AsteroidZone
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddResponseCompression();
             services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseResponseCompression();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
