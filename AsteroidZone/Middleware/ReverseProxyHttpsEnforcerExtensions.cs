267478b520d9959f37cdd042c0e40f2c34bace38
Milen Georgiev <milendg99@gmail.com>
2021-05-16 02:40:28 +0300

    Added documentation to ReverseProxyHttpsEnforcerExtensions.cs

--- AsteroidZone/Middleware/ReverseProxyHttpsEnforcerExtensions.cs
+++ AsteroidZone/Middleware/ReverseProxyHttpsEnforcerExtensions.cs
@@ -22,15 +22,23 @@ namespace AsteroidZone.Middleware
             _next = next;
         }
 
+        /// <summary>
+        /// Enforces HTTPS when opening the website
+        /// </summary>
+        /// <param name="ctx">HTTP context of the request</param>
+        /// <returns>Async Task</returns>
         public async Task Invoke(HttpContext ctx)
         {
+            // Check the headers and if the client is not using HTTPS, enforce it
             var h = ctx.Request.Headers;
             if (h[ForwardedProtoHeader] == string.Empty || h[ForwardedProtoHeader] == "https")
             {
+                // Simply continue
                 await _next(ctx);
             }
             else if (h[ForwardedProtoHeader] != "https")
             {
+                // Get the URL with HTTPS and redirect to it
                 var withHttps = $"https://{ctx.Request.Host}{ctx.Request.Path}{ctx.Request.QueryString}";
                 ctx.Response.Redirect(withHttps);
             }
